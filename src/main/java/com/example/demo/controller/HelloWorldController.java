f8012aa8c0cf9814ee7d3d41b7ef7f525569e32e
SOG\Makara.Bhatamrekar <makarand.bhatamrekar@dhs.ga.gov>
2022-07-01 11:17:29 -0400

    initial commit

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders ="**" )
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public TestCmd  helloWorld(){
+        TestCmd cmd = new TestCmd();
+        cmd.setMessage("hello world");
+
+        return cmd;
+    }
+}
+
+class TestCmd{
+    private String message;
+
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+}
